8992862f2c478e2913425e470f976042c366dc63
Wazt <112556610+WaztWolf@users.noreply.github.com>
2025-09-02 20:45:31 +0200

    Create VoicebankConfigModel.cs

---
+++ Models/VoicebankConfigModel.cs
@@ -0,0 +1,16 @@
+namespace OkamySynthW.Models
+{
+    public class VoiceInfo
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Version { get; set; } = string.Empty;
+        public string EditorVersion { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Dictionary { get; set; } = string.Empty;
+        public string DictionaryPath { get; set; } = string.Empty;
+        public string Samples { get; set; } = string.Empty;
+        public string PitchData { get; set; } = string.Empty;
+        public string ImgSample { get; set; } = string.Empty;
+        public string PUID { get; set; } = string.Empty;
+    }
+}
